1c73ff01007d1bf9a3f661d1bb757fa9e22343bc
Machado Gamboa, Kevin <km20329@essex.ac.uk>
2021-03-24 11:39:06 +0000

    finishing exercise

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ with open('Models/pickle_MLP_model.pkl', 'rb') as f:
     svm_model = pickle.load(f)
 
 
-def get_predictions(price, Tax, Driver_Age, Licence_Length_Years, req_model):
-    mylist = [Driver_Age, Tax, price, Licence_Length_Years]
+def get_predictions(age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal,req_model):
+    mylist = [age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal]
     mylist = [float(i) for i in mylist]
     vals = [mylist]
 
@@ -47,18 +47,31 @@ def homepage():
 @app.route('/', methods=['POST', 'GET'])
 def my_form_post():
     if request.method == 'POST':
-        price = request.form['price']
-        Tax = request.form['Tax']
-        Driver_Age = request.form['Driver_Age']
-        Licence_Length_Years = request.form['Licence_Length_Years']
+        age = request.form['Age']
+        sex = request.form['sex']
+        cp = request.form['cp']
+        trestbps = request.form['trestbps']
+
+        chol = request.form['chol']
+        fbs = request.form['fbs']
+        restecg = request.form['restecg']
+        thalach = request.form['thalach']
+
+        exang = request.form['exang']
+        oldpeak = request.form['oldpeak']
+        slope = request.form['slope']
+        ca = request.form['ca']
+        thal = request.form['thal']
+
         req_model = request.form['req_model']
 
-        target = get_predictions(price, Tax, Driver_Age, Licence_Length_Years, req_model)
+        target = get_predictions(age,sex,cp,trestbps,chol,fbs,restecg,thalach,
+                                 exang,oldpeak,slope,ca,thal, req_model)
 
         if target==1:
-            sale_making = 'Customer is likely to buy the insurance'
+            sale_making = 'Potential heart disease detected .. Go see a doctor'
         else:
-            sale_making = 'Customer is unlikely to buy the insurance'
+            sale_making = 'unlikely to have a heart disease .. keep exercising'
 
         return render_template('home.html', target = target, sale_making = sale_making)
     else:
